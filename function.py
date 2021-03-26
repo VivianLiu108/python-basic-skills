291670027f665aa5df4ece88da39cdd27cea042c
VivianLiu108 <61656910+VivianLiu108@users.noreply.github.com>
2021-03-26 18:30:53 +0800

    Update function.py

--- function.py
+++ function.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[19]:
-
-
 def avg(x, y):
     sum = x + y
     return sum/2
@@ -12,22 +6,3 @@ def main():
     print(avg(3, 5))
 
 main()
-
-
-# In[9]:
-
-
-
-
-
-# In[17]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
