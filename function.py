a2a2e590ed93878e306227d3799505e36766b151
VivianLiu108 <61656910+VivianLiu108@users.noreply.github.com>
2021-03-26 18:30:03 +0800

    Add files via upload

---
+++ function.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+def avg(x, y):
+    sum = x + y
+    return sum/2
+
+def main():
+    print(avg(3, 5))
+
+main()
+
+
+# In[9]:
+
+
+
+
+
+# In[17]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
