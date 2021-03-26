a510348d961e1eacef336cec712c77268eaf8a95
VivianLiu108 <61656910+VivianLiu108@users.noreply.github.com>
2021-03-26 14:31:48 +0800

    Add files via upload

---
+++ inputStringInt.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+a = input()   #輸入 type為string
+b = input()
+print(a + b)  #為string ex.a = 1, b = 2, 輸出 12
+
+c = eval(input())  #eval(str) 將字串str當成有效表達式來求值並返回計算結果
+d = eval(input())
+print(c + d)       #為int ex.a = 1, b = 2, 輸出 3
+
+
+# In[ ]:
+
+
+
+
