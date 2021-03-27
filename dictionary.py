8cb41989c63cf7493192a67f72a7333b1633cccf
VivianLiu108 <61656910+VivianLiu108@users.noreply.github.com>
2021-03-27 20:23:10 +0800

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[27]:
+
+
+amount = {}
+amount["apple"] = 5
+amount["banana"] = 6
+amount["apple"] += 3
+print(amount)           #{'apple': 8, 'banana': 6}
+for i in amount:        #apple
+    print(i)            #banana
+print(amount["apple"])  #8
+print(amount["apple"] + amount["banana"]) #14
+
+nameList = {}
+nameList["a"] = "Alice"
+nameList["b"] = "Bob"
+nameList["c"] = "Cindy"
+for name in nameList:
+    print(nameList[name])
+print(nameList["a"] + " " + nameList["b"])
+#Alice
+#Bob
+#Cindy
+#Alice Bob
+
+
+# In[8]:
+
+
+
+
+
+# In[17]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
